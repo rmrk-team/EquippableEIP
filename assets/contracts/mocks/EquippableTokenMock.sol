1971125386f7a3428c80861261d9f4e5e1b8d00a
steven2308 <steven2308@outlook.com>
2022-12-21 12:14:09 -0500

    Adds contracts for base implementation.

---
+++ assets/contracts/mocks/EquippableTokenMock.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: CC0-1.0
+
+pragma solidity ^0.8.16;
+
+import "../EquippableToken.sol";
+
+//Minimal public implementation of INestable for testing.
+contract EquippableTokenMock is EquippableToken {
+    constructor() EquippableToken() {}
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+
+    function nestMint(
+        address to,
+        uint256 tokenId,
+        uint256 destinationId
+    ) external {
+        _nestMint(to, tokenId, destinationId, "");
+    }
+
+    // Utility transfers:
+
+    function transfer(address to, uint256 tokenId) public virtual {
+        transferFrom(_msgSender(), to, tokenId);
+    }
+
+    function nestTransfer(
+        address to,
+        uint256 tokenId,
+        uint256 destinationId
+    ) public virtual {
+        nestTransferFrom(_msgSender(), to, tokenId, destinationId, "");
+    }
+
+    function addAssetToToken(
+        uint256 tokenId,
+        uint64 assetId,
+        uint64 replacesAssetWithId
+    ) external {
+        _addAssetToToken(tokenId, assetId, replacesAssetWithId);
+    }
+
+    function addEquippableAssetEntry(
+        uint64 id,
+        uint64 equippableGroupId,
+        address baseAddress,
+        string memory metadataURI,
+        uint64[] calldata partIds
+    ) external {
+        _addAssetEntry(
+            id,
+            equippableGroupId,
+            baseAddress,
+            metadataURI,
+            partIds
+        );
+    }
+
+    function setValidParentForEquippableGroup(
+        uint64 equippableGroupId,
+        address parentAddress,
+        uint64 partId
+    ) external {
+        _setValidParentForEquippableGroup(
+            equippableGroupId,
+            parentAddress,
+            partId
+        );
+    }
+}
